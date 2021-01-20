feee11b33c6e7b1d8d914aa931d4aeef05cb4d80
Erik Pettersson <eripe970@gmail.com>
2021-01-20 20:15:28 +0100

    first draft version

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/eripe970/pubsub-signing
+
+go 1.15
+
+require (
+	cloud.google.com/go/pubsub v1.9.1
+	google.golang.org/api v0.36.0
+)
---
+++ signing.go
@@ -0,0 +1,89 @@
+package signing
+
+import (
+	pubsub2 "cloud.google.com/go/pubsub"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"google.golang.org/api/pubsub/v1"
+)
+
+const (
+	signatureAttribute = "signature"
+)
+
+var (
+	ErrInvalidSignature = errors.New("message has an invalid signature")
+	ErrNotSigned        = errors.New("message does not have a signature")
+)
+
+type PushMessage struct {
+	Message      pubsub.PubsubMessage `json:"message"`
+	Subscription string               `json:"subscription"`
+}
+
+func ConstructMessage(payload []byte, secret string) (*PushMessage, error) {
+	var m PushMessage
+
+	if err := json.Unmarshal(payload, &m); err != nil {
+		return nil, fmt.Errorf("failed to parse message json: %s", err.Error())
+	}
+
+	// Data from pub/sub is base64 encoded
+	data, err := base64.StdEncoding.DecodeString(m.Message.Data)
+
+	if err != nil {
+		return nil, err
+	}
+
+	signature := m.Message.Attributes[signatureAttribute]
+
+	if signature == "" {
+		return nil, ErrNotSigned
+	}
+
+	err = validateSignature(data, secret, signature)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &m, err
+}
+
+func validateSignature(payload []byte, secret string, signature string) error {
+	mac := hmac.New(sha256.New, []byte(secret))
+	mac.Write(payload)
+
+	sign := mac.Sum(nil)
+
+	decodedSign, err := hex.DecodeString(signature)
+
+	if err != nil {
+		return err
+	}
+
+	if hmac.Equal(decodedSign, sign) {
+		return nil
+	}
+
+	return ErrInvalidSignature
+}
+
+func SignMessage(message *pubsub2.Message, secret string) error {
+	signature := computeSignatureWithKey(message.Data, secret)
+
+	message.Attributes[signatureAttribute] = hex.EncodeToString(signature)
+
+	return nil
+}
+
+func computeSignatureWithKey(payload []byte, secret string) []byte {
+	mac := hmac.New(sha256.New, []byte(secret))
+	mac.Write(payload)
+	return mac.Sum(nil)
+}
